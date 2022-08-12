ffaafbc6f010c3360bccf50978e64780b97c7066
Dan <webdevd4n@gmail.com>
2022-08-12 18:53:11 +0900

    Finished restyling MainPageHeader component

--- src/reusable/headers/MainPageHeader/MainPageHeader.js
+++ src/reusable/headers/MainPageHeader/MainPageHeader.js
@@ -4,7 +4,9 @@ import './MainPageHeader.css';
 function MainPageHeader({ currentPageName, iconClasses }) {
     return (
         <div className="main-content__header">
-            <i className={`main-content__header--icon ${iconClasses}`}></i>
+            <div className="main-content__header--icon-container">
+                <i className={`main-content__header--icon ${iconClasses}`}></i>
+            </div>
             <h2 className="main-content__header--header">{currentPageName}</h2>
         </div>
     )
