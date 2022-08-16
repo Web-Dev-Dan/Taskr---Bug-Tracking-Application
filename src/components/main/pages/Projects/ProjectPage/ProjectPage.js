652f7a412c6006af85f550699fd387e61a7f98af
Dan <webdevd4n@gmail.com>
2022-08-17 00:39:46 +0900

    Added task categories to ProjectPage.js component and conditional EmptyNotificiation.js component

--- src/components/main/pages/Projects/ProjectPage/ProjectPage.js
+++ src/components/main/pages/Projects/ProjectPage/ProjectPage.js
@@ -1,5 +1,6 @@
 
 import { useState, useEffect } from 'react';
+import EmptyNotification from '../../../../../reusable/notifications/EmptyNotification/EmptyNotification';
 import './ProjectPage.css';
 
 function ProjectPage({ userData, currentProjectId, project, updateInputValue }) {
@@ -13,13 +14,84 @@ function ProjectPage({ userData, currentProjectId, project, updateInputValue }) 
                 </div>
             </div>
 
-            {/* {userData.projects.map(project => {
-                if (project.id === currentProjectId) {
-                    return <div>
-                        <h1>{project.title}</h1>
+            {/* Project List Content */}
+            <div className="main__content">
+                <div className="main__list">
+                    {/* List Header */}
+                    <div className="main__list-header">
+                        <i className="main__list-header--icon fa-solid fa-layer-group color--primary"></i>
+                        <div className="main__list-header--text">
+                            <h3 className="list-header__title">To Do</h3>
+                            <p className="list-header__subtitle">{project.tasks.toDo.length} {project.tasks.toDo.length === 1 ? 'task' : 'tasks'} remaining</p>
+                        </div>
                     </div>
-                }
-            })} */}
+                    {/* List Button */}
+                    <button className="list__add-btn btn">
+                        <i className="list__add-btn--icon fa-solid fa-circle-plus"></i>
+                        <p className="list__add-btn--text">Add Task</p>
+                    </button>
+                    {/* List */}
+                    <div className="list list--to-do">
+                        {/* List Items */}
+                        {project.tasks.toDo.length === 0 && <EmptyNotification
+                            icon='fa-solid fa-box-open'
+                            text='Looks like this list is empty!'
+                        />}
+
+                    </div>
+                </div>
+
+                <div className="main__list">
+                    {/* List Header */}
+                    <div className="main__list-header">
+                        <i className="main__list-header--icon fa-solid fa-trowel-bricks color--yellow"></i>
+                        <div className="main__list-header--text">
+                            <h3 className="list-header__title">In Progress</h3>
+                            <p className="list-header__subtitle">{project.tasks.inProgress.length} {project.tasks.inProgress.length === 1 ? 'task' : 'tasks'} remaining</p>
+                        </div>
+                    </div>
+                    {/* List Button */}
+                    <button className="list__add-btn btn">
+                        <i className="list__add-btn--icon fa-solid fa-circle-plus"></i>
+                        <p className="list__add-btn--text">Add Task</p>
+                    </button>
+                    {/* List */}
+                    <div className="list list--in-progress">
+                        {/* List Items */}
+                        {project.tasks.inProgress.length === 0 && <EmptyNotification
+                            icon='fa-solid fa-box-open'
+                            text='Looks like this list is empty!'
+                        />}
+
+                    </div>
+                </div>
+
+                <div className="main__list">
+                    {/* List Header */}
+                    <div className="main__list-header">
+                        <i className="main__list-header--icon fa-solid fa-fire color--green"></i>
+                        <div className="main__list-header--text">
+                            <h3 className="list-header__title">Complete</h3>
+                            <p className="list-header__subtitle">{project.tasks.complete.length} {project.tasks.complete.length === 1 ? 'task' : 'tasks'} remaining</p>
+                        </div>
+                    </div>
+                    {/* List Button */}
+                    <button className="list__add-btn btn">
+                        <i className="list__add-btn--icon fa-solid fa-circle-plus"></i>
+                        <p className="list__add-btn--text">Add Task</p>
+                    </button>
+                    {/* List */}
+                    <div className="list list--complete">
+                        {/* List Items */}
+                        {project.tasks.complete.length === 0 && <EmptyNotification
+                            icon='fa-solid fa-box-open'
+                            text='Looks like this list is empty!'
+                        />}
+
+                    </div>
+                </div>
+            </div>
+
 
 
         </div >
